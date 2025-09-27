d6f2412eb8c3423a65c82905670e33b0145dbaa6
Rainerino <albertyanyy@gmail.com>
2025-09-27 17:22:48 +0800

    up

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=open("requirements.txt", encoding="utf-8").read().splitlines(),
 )
